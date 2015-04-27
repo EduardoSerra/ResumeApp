518a71d1a0fd3507df6210f57eeca1220b23435e
Eduardo Serra <Eduardojavierserra@gmail.com>
2015-04-27 10:42:25 -0400

    First commit
    Added customAPI.txt, index.html, scripts.js, and styles.css

---
+++ scripts.js
@@ -0,0 +1,25 @@
+﻿//GET DATA FROM MOBILE SERVICE CUSTOM API AND BIND TO KENDO UI VIEWMODEL
+$(function () {
+    var client = new WindowsAzure.MobileServiceClient(
+        "https://resume42.azure-mobile.net/",
+        "tkVzDgMHBDTPKsuZliJuqwWIrFoEiF34"
+    );
+    client.invokeApi("getResume", {
+        body: null,
+        method: "get"
+    }).done(function (results) {
+        var resume = results.result;
+        var viewModel = kendo.observable({
+            name: resume.name,
+            address: resume.address,
+            phone: resume.phone,
+            linkedIn: resume.linkedIn,
+            email: resume.email,
+            emailLink: "mailto:" + resume.email,
+            workExperienceItems: resume.workExperience,
+            educationItems: resume.education,
+            skillItems: resume.skills
+        });
+        kendo.bind($("#paper"), viewModel);
+    });
+})
